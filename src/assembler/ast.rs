f3228fc4c1b4d16592d0a5945ad9d571fabd1160
SimTheFool <simon.sauzede@gmail.com>
2023-11-17 14:01:42 +0100

    feat: parse metas

---
+++ src/assembler/ast.rs
@@ -0,0 +1,223 @@
+use crate::utils::result::AppError;
+use crate::utils::result::AppResult;
+use pest::Parser;
+use pest::Span;
+use pest_ast::FromPest;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "assembler/ttl.pest"]
+struct TTLParser;
+
+fn span_into_string(span: Span) -> String {
+    span.as_str().to_string()
+}
+fn span_into_str(span: Span) -> &str {
+    span.as_str()
+}
+
+#[derive(Debug, PartialEq, FromPest)]
+#[pest_ast(rule(Rule::identifier))]
+pub struct Variable(#[pest_ast(outer(with(span_into_string)))] pub String);
+
+#[derive(Debug, PartialEq, FromPest)]
+#[pest_ast(rule(Rule::reference))]
+pub struct Ref(#[pest_ast(outer(with(span_into_string)))] pub String);
+
+#[derive(Debug, PartialEq, FromPest)]
+#[pest_ast(rule(Rule::string))]
+pub struct StringLit(#[pest_ast(inner(with(span_into_string)))] pub String);
+
+#[derive(Debug, PartialEq, FromPest)]
+#[pest_ast(rule(Rule::number))]
+pub struct Number(
+    #[pest_ast(outer(with(span_into_str), with(str::parse::<f64>), with(Result::unwrap)))] pub f64,
+);
+
+#[derive(Debug, PartialEq, FromPest)]
+#[pest_ast(rule(Rule::object))]
+pub struct Object(pub Vec<Declaration>);
+
+#[derive(Debug, PartialEq, FromPest)]
+#[pest_ast(rule(Rule::meta))]
+pub enum Meta {
+    String(StringLit),
+    Number(Number),
+}
+
+#[derive(Debug, PartialEq, FromPest)]
+#[pest_ast(rule(Rule::metas))]
+pub struct Metas(pub Vec<Meta>);
+impl Into<Vec<String>> for Metas {
+    fn into(self) -> Vec<String> {
+        let mut res = Vec::new();
+        for meta in self.0 {
+            match meta {
+                Meta::String(s) => res.push(s.0),
+                Meta::Number(n) => res.push(n.0.to_string()),
+            }
+        }
+        res
+    }
+}
+
+#[derive(Debug, PartialEq, FromPest)]
+#[pest_ast(rule(Rule::value))]
+pub enum Value {
+    String(StringLit, Option<Metas>),
+    Number(Number, Option<Metas>),
+    Object(Object),
+}
+
+#[derive(Debug, PartialEq, FromPest)]
+#[pest_ast(rule(Rule::declaration))]
+pub struct Declaration {
+    pub identifier: Variable,
+    pub value: Value,
+}
+
+#[derive(Debug, FromPest)]
+#[pest_ast(rule(Rule::EOI))]
+struct EOI;
+
+#[derive(Debug, FromPest)]
+#[pest_ast(rule(Rule::file))]
+pub struct File {
+    pub value: Value,
+    _eoi: EOI,
+}
+impl TryFrom<&str> for File {
+    type Error = AppError;
+
+    fn try_from(s: &str) -> AppResult<Self> {
+        use from_pest::FromPest;
+        let mut pairs =
+            TTLParser::parse(Rule::file, s).map_err(|e| AppError::String(e.to_string()))?;
+        let file = File::from_pest(&mut pairs).map_err(|e| AppError::String(e.to_string()))?;
+        Ok(file)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Declaration, Variable};
+    use from_pest::FromPest;
+    use pest::Parser;
+
+    #[test]
+    fn it_should_parse_identifier() {
+        let str = "var01";
+        let mut pairs = super::TTLParser::parse(super::Rule::identifier, str).unwrap();
+        let variable = Variable::from_pest(&mut pairs).unwrap();
+
+        assert_eq!(variable.0, "var01");
+    }
+
+    #[test]
+    fn it_should_parse_declaration() {
+        let str = r#"var01: 745 ["meta1" "01" 15]"#;
+        let mut pairs = super::TTLParser::parse(super::Rule::declaration, str).unwrap();
+        let declaration = Declaration::from_pest(&mut pairs).unwrap();
+
+        let identifier = declaration.identifier;
+        let value = declaration.value;
+        let (value, metas) = match value {
+            super::Value::Number(m, n) => (m.0, n),
+            _ => panic!("Unexpected value"),
+        };
+
+        assert_eq!(identifier.0, "var01");
+        assert_eq!(value, 745.0);
+
+        let metas = metas.unwrap().0;
+        assert_eq!(metas.len(), 3);
+        match metas.get(0).unwrap() {
+            super::Meta::String(s) => assert_eq!(s.0, "meta1"),
+            _ => panic!("Unexpected meta"),
+        }
+        match metas.get(1).unwrap() {
+            super::Meta::String(s) => assert_eq!(s.0, "01"),
+            _ => panic!("Unexpected meta"),
+        }
+        match metas.get(2).unwrap() {
+            super::Meta::Number(n) => assert_eq!(n.0, 15.0),
+            _ => panic!("Unexpected meta"),
+        }
+    }
+
+    #[test]
+    fn it_should_parse_object() {
+        let str = r#"{
+            var02: 745
+            var03: "hello"
+        }"#;
+
+        let mut pairs = super::TTLParser::parse(super::Rule::object, str).unwrap();
+        let object = super::Object::from_pest(&mut pairs).unwrap();
+
+        let declarations = object.0;
+
+        assert_eq!(declarations.len(), 2);
+
+        let first_declaration = declarations.get(0).unwrap();
+        let first_var = &first_declaration.identifier;
+        let first_value = &first_declaration.value;
+        let first_value = match first_value {
+            super::Value::Number(n, _) => n.0,
+            _ => panic!("Unexpected value"),
+        };
+
+        let second_declaration = declarations.get(1).unwrap();
+        let second_var = &second_declaration.identifier;
+        let second_value = &second_declaration.value;
+        let second_value = match second_value {
+            super::Value::String(s, _) => s.0.clone(),
+            _ => panic!("Unexpected value"),
+        };
+
+        assert_eq!(first_var.0, "var02");
+        assert_eq!(first_value, 745.0);
+        assert_eq!(second_var.0, "var03");
+        assert_eq!(second_value, "hello");
+    }
+
+    #[test]
+    fn it_should_parse_file() {
+        let str = r#"
+        {
+            var02: 745
+            var03: "hello"
+        }
+        "#;
+
+        let mut pairs = super::TTLParser::parse(super::Rule::file, str).unwrap();
+        let file = super::File::from_pest(&mut pairs).unwrap();
+        let value = file.value;
+
+        let declarations = match value {
+            super::Value::Object(s) => s.0,
+            _ => panic!("Unexpected value"),
+        };
+
+        let first_declaration = declarations.get(0).unwrap();
+        let first_var = &first_declaration.identifier;
+        let first_value = &first_declaration.value;
+        let first_value = match first_value {
+            super::Value::Number(n, _) => n.0,
+            _ => panic!("Unexpected value"),
+        };
+
+        let second_declaration = declarations.get(1).unwrap();
+        let second_var = &second_declaration.identifier;
+        let second_value = &second_declaration.value;
+        let second_value = match second_value {
+            super::Value::String(s, _) => s.0.clone(),
+            _ => panic!("Unexpected value"),
+        };
+
+        assert_eq!(first_var.0, "var02");
+        assert_eq!(first_value, 745.0);
+        assert_eq!(second_var.0, "var03");
+        assert_eq!(second_value, "hello");
+    }
+}
--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,11 +1,9 @@
-pub mod parser;
+pub mod ast;
 
 use crate::{
-    assembler::parser::ast::{self, Value},
-    utils::result::{AppError, AppResult},
+    assembler::ast::{File, Value},
+    utils::result::AppResult,
 };
-use from_pest::FromPest;
-use pest::Parser;
 
 #[derive(PartialEq, Debug)]
 struct TTLResource<T> {
@@ -15,16 +13,12 @@ struct TTLResource<T> {
 }
 
 impl<T> TTLResource<T> {
-    fn new(value: T, identifier: String) -> Self {
+    fn new(value: T, identifier: String, metas: Option<Vec<String>>) -> Self {
         Self {
             identifier,
             value,
-            metas: Vec::new(),
+            metas: metas.unwrap_or_else(|| Vec::new()),
         }
-    }
-
-    fn add_meta(&mut self, meta: String) {
-        self.metas.push(meta);
     }
 }
 
@@ -36,21 +30,20 @@ enum TTLResources {
 }
 
 fn assemble_file(file_str: &str) -> AppResult<Vec<TTLResources>> {
-    let mut pairs = parser::TTLParser::parse(parser::Rule::file, file_str)
-        .map_err(|e| AppError::String(e.to_string()))?;
-
-    let file = ast::File::from_pest(&mut pairs).map_err(|e| AppError::String(e.to_string()))?;
+    let file = File::try_from(file_str)?;
     let value = file.value;
 
-    fn ast_to_value(val: ast::Value, path: Option<String>) -> Vec<TTLResources> {
+    fn ast_to_value(val: Value, path: Option<String>) -> Vec<TTLResources> {
         match val {
-            Value::String(s) => vec![TTLResources::String(TTLResource::new(
+            Value::String(s, metas) => vec![TTLResources::String(TTLResource::new(
                 s.0,
                 path.unwrap_or_else(|| "".to_string()),
+                metas.map(|m| m.into()),
             ))],
-            Value::Number(n) => vec![TTLResources::Number(TTLResource::new(
+            Value::Number(n, metas) => vec![TTLResources::Number(TTLResource::new(
                 n.0,
                 path.unwrap_or_else(|| "".to_string()),
+                metas.map(|m| m.into()),
             ))],
             Value::Object(o) => {
                 let mut res = Vec::new();
@@ -59,13 +52,12 @@ fn assemble_file(file_str: &str) -> AppResult<Vec<TTLResources>> {
                         Some(ref p) => {
                             let mut new_path = p.to_string();
                             new_path.push_str(".");
-                            new_path.push_str(&decl.0 .0);
+                            new_path.push_str(&decl.identifier.0);
                             Some(new_path)
                         }
-                        None => Some(decl.0 .0.to_string()),
+                        None => Some(decl.identifier.0.to_string()),
                     };
-
-                    res.extend(ast_to_value(decl.1, new_path));
+                    res.extend(ast_to_value(decl.value, new_path));
                 }
                 res
             }
@@ -86,7 +78,7 @@ mod tests {
     fn it_should_create_resource() {
         let values = assemble_file(
             r#"{
-                var04: 745
+                var04: 745 ["abc" "def" 123]
                 var05: "hello"
                 var06: {
                     var07: 07
@@ -97,10 +89,22 @@ mod tests {
         .unwrap();
 
         let expected_values = vec![
-            TTLResources::Number(TTLResource::new(745.0, "var04".to_string())),
-            TTLResources::String(TTLResource::new("hello".to_string(), "var05".to_string())),
-            TTLResources::Number(TTLResource::new(7.0, "var06.var07".to_string())),
-            TTLResources::Number(TTLResource::new(8.0, "var06.var08".to_string())),
+            TTLResources::Number(TTLResource::new(
+                745.0,
+                "var04".to_string(),
+                Some(vec![
+                    "abc".to_string(),
+                    "def".to_string(),
+                    "123".to_string(),
+                ]),
+            )),
+            TTLResources::String(TTLResource::new(
+                "hello".to_string(),
+                "var05".to_string(),
+                None,
+            )),
+            TTLResources::Number(TTLResource::new(7.0, "var06.var07".to_string(), None)),
+            TTLResources::Number(TTLResource::new(8.0, "var06.var08".to_string(), None)),
         ];
 
         assert_eq!(values, expected_values);
